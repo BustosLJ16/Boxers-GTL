db40e3e11abaeefeb2244f2a3cc2a21b08478bd1
bustoslj16 <bustoslj16@gmail.com>
2026-08-26 19:11:59 -0300

    V1.4: Agrego DB con Firebase/Firestore y cambio controles de datos.

--- src/Data/firebase.js
+++ src/Data/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxiZtsVECsOcg61l6IAjtmvuI_k5sZEbs",
-    authDomain: "boxers-gtl-proyecto.firebaseapp.com",
-    projectId: "boxers-gtl-proyecto",
-    storageBucket: "boxers-gtl-proyecto.firebasestorage.app",
-    messagingSenderId: "291583569623",
-    appId: "1:291583569623:web:bd8c2de514a158bdce8a92",
-    measurementId: "G-DTCZLCY702"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
